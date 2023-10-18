bf1fc5a6c2f9dc95b87d722c5fa54019fab158ac
kilev.do <kilev.do@reliab.tech>
2023-10-18 23:01:57 +0300

    Initial commit

---
+++ src/main/java/tech/reliab/course/toropchinda/BankApplication.java
@@ -0,0 +1,13 @@
+package tech.reliab.course.toropchinda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/reliab/course/toropchinda/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.reliab.course.toropchinda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
